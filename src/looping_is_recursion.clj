08fc5de5561006059e9e45b7cd3093f2e2d39cd4
Markus Juopperi <markus.juopperi@kolumbus.fi>
2016-10-24 19:36:06 +0300

    Solve seq=

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -15,7 +15,12 @@
     (last-acc nil a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (cond
+    (and (empty? seq1) (empty? seq2)) true
+    (empty? seq1)                     false
+    (empty? seq2)                     false
+    (not= (first seq1) (first seq2))  false
+    :else (recur (rest seq1) (rest seq2))))
 
 (defn find-first-index [pred a-seq]
   ":(")
